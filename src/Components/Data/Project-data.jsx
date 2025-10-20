a63ca8950ef2cff8b852624c622b6bbfc40aa515
kryptune <51198180+kryptune@users.noreply.github.com>
2025-10-21 02:59:10 +0800

    add new project

--- src/Components/Data/Project-data.jsx
+++ src/Components/Data/Project-data.jsx
@@ -1,62 +1,75 @@
-const project = 
-[   
-    {
-        id: 0,
-        title: "WeatherNow App",
-        description: "WeatherNow is a modern weather app that gives you up-to-date forecasts with a simple, intuitive interface.",
-        link: "https://weather-app-chi-seven-11.vercel.app/",
-        tags: ["React", "JavaScript", "Tailwind", "API", "localStorage" ],
-        image: "/weather-app.PNG"
-    },
-    {
-        id: 1,
-        title: "Portfolio Website",
-        description: "My personal portfolio website showcasing my projects and skills.",
-        link: "https://kryptune-portfolio.netlify.app/",
-        tags: ["React", "JavaScript", "CSS", "HTML"],
-        image: "/portfolio-website.PNG"
-    },
-    {
-        id: 2,
-        title: "Blackjack App",
-        description: "A simple blackjack game where you can play against the dealer.",
-        link: "https://blckjck2.netlify.app/",
-        tags: ["JavaScript", "CSS", "Python", "PostgreSQL", "Flask"],
-        image: "/blackjack-app.PNG"
-    },
-    {
-        id: 3,
-        title: "Tenzies App",
-        description: "A fun dice game where you try to roll all dice to the same number.",
-        link: "https://tenzies-app-omega.vercel.app/",
-        tags: ["React", "JavaScript", "Tailwind", "localStorage"],
-        image: "/tenzies-app.PNG"
-    },
-    {
-        id: 4,
-        title: "React Notes",
-        description: "A note-taking app built with React and Firebase, allowing you to create and manage notes.",
-        link: "https://react-notes-gamma.vercel.app/",
-        tags: ["React", "Firebase", "JavaScript", "CSS"],
-        image: "/notes-app.PNG"
-
-    },
-    {   
-        id: 5,
-        title: "Airbnb Clone",
-        description: "A clone of the Airbnb website, built with React and styled-components.",
-        link: "https://airbnb-app-tawny-eight.vercel.app/",
-        tags: ["React", "JavaScript", "CSS", "Styled-Components"],
-        image: "/airbnb-app.PNG"
-    },
-    {
-        id: 6,
-        title: "Meme App",
-        description: "An app that generates random memes using the Meme API.",
-        link: "https://meme-app-theta.vercel.app/",
-        tags: ["React", "JavaScript", "CSS"],
-        image: "/meme-app.PNG"
-    }
-]
+const project = [
+  {
+    id: 0,
+    title: "Marci Realty",
+    description:
+      "A client-facing web app replica built to showcase front-end proficiency, reusable components, and optimized performance.",
+    link: "https://weather-app-chi-seven-11.vercel.app/",
+    tags: ["React", "Tailwind", "Framer-motion", "Swiper"],
+    image: "/marci.PNG",
+  },
+  {
+    id: 1,
+    title: "WeatherNow App",
+    description:
+      "WeatherNow is a modern weather app that gives you up-to-date forecasts with a simple, intuitive interface.",
+    link: "https://weather-app-chi-seven-11.vercel.app/",
+    tags: ["React", "JavaScript", "Tailwind", "API", "localStorage"],
+    image: "/weather-app.PNG",
+  },
+  {
+    id: 2,
+    title: "Portfolio Website",
+    description:
+      "My personal portfolio website showcasing my projects and skills.",
+    link: "https://kryptune-portfolio.netlify.app/",
+    tags: ["React", "JavaScript", "CSS", "HTML"],
+    image: "/portfolio-website.PNG",
+  },
+  {
+    id: 3,
+    title: "Blackjack App",
+    description:
+      "A simple blackjack game where you can play against the dealer.",
+    link: "https://blckjck2.netlify.app/",
+    tags: ["JavaScript", "CSS", "Python", "PostgreSQL", "Flask"],
+    image: "/blackjack-app.PNG",
+  },
+  {
+    id: 4,
+    title: "Tenzies App",
+    description:
+      "A fun dice game where you try to roll all dice to the same number.",
+    link: "https://tenzies-app-omega.vercel.app/",
+    tags: ["React", "JavaScript", "Tailwind", "localStorage"],
+    image: "/tenzies-app.PNG",
+  },
+  {
+    id: 5,
+    title: "React Notes",
+    description:
+      "A note-taking app built with React and Firebase, allowing you to create and manage notes.",
+    link: "https://react-notes-gamma.vercel.app/",
+    tags: ["React", "Firebase", "JavaScript", "CSS"],
+    image: "/notes-app.PNG",
+  },
+  {
+    id: 6,
+    title: "Airbnb Clone",
+    description:
+      "A clone of the Airbnb website, built with React and styled-components.",
+    link: "https://airbnb-app-tawny-eight.vercel.app/",
+    tags: ["React", "JavaScript", "CSS", "Styled-Components"],
+    image: "/airbnb-app.PNG",
+  },
+  {
+    id: 7,
+    title: "Meme App",
+    description: "An app that generates random memes using the Meme API.",
+    link: "https://meme-app-theta.vercel.app/",
+    tags: ["React", "JavaScript", "CSS"],
+    image: "/meme-app.PNG",
+  },
+];
 
 export default project;
